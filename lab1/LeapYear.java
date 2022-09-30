2f83e664e56739973458b285852a2d81a31c9151
xiangddang <67621584+xiangddang@users.noreply.github.com>
2022-09-30 21:58:00 +0800

    revise (

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -15,7 +15,7 @@ public class LeapYear {
         }
     }
     public static boolean isLeapYear(int year) {
-	if (year % 400 == 0 || (year % 4 == 0 & year % 100 != 0){
+	if (year % 400 == 0 || (year % 4 == 0 & year % 100 != 0)){
 		return true;
 	}else{
 		return false;
